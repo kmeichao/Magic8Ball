d99442fd8559698f7e39e2910c118a8e93823472
kmeichao <kayleemeichao@gmail.com>
2022-06-30 15:36:49 -0700

    new magic 8 ball project

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var messageLabel: UILabel!
+    
+    var messages = ["The answer points to no",
+                    "today is not your day...",
+                    "suck it up and pucker up",
+                    "It is certain",
+                    "Outlook is good",
+                    "Don't count on it",
+                    "Odds are slim",
+                    "Unclear...just like your skin",
+                    "Yes"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        messageLabel.text = ""
     }
 
-
+    @IBAction func questionButtonPressed(_ sender: UIButton) {
+        let chosenMessages = messages.randomElement()
+        messageLabel.text = chosenMessages
+    }
+    
 }
 
