d17e0a82570e9a6fcbcb8396ae63b63b7c10c887
kmeichao <kayleemeichao@gmail.com>
2022-06-30 14:41:35 -0700

    Initial Commit

---
+++ Magic8Ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Magic8Ball
+//
+//  Created by Kaylee Mei Chao on 6/30/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
